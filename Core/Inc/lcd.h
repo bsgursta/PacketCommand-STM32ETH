5cad632974b5d947cd47a54d23b58d05bc5ace59
bsgursta <bryansgonzalez156@gmail.com>
2025-07-18 13:44:02 -0400

    new

--- Core/Inc/lcd.h
+++ Core/Inc/lcd.h
@@ -8,6 +8,8 @@
 
 void wire4SPI_init(void);
 void lcd_reset(void);
+void lcd_setcolumn_address(int column_num);
+void lcd_setpage_address(int page_num);
 
 
 
--- Core/Src/lcd.c
+++ Core/Src/lcd.c
@@ -5,6 +5,29 @@
 
 //Reset lcd settings
 void lcd_reset(void){
+	//pull RES pin down
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
+}
+
+void lcd_write(uint8)
+
+//set the column address for writing pixels
+void lcd_setcolumn_address(int column_num){
+
+}
+
+//set the page address for writing pixels
+//page number must be between 0-15
+void lcd_setpage_address(int page_num){
+	//convert int to uint8
+	uint8_t pg_num = page_num;
+	//data byte for page address
+	uint8_t data = 0xB0 | pg_num;
+
+	//pull A0 low (PD7)
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_7, GPIO_PIN_RESET);
+
+
 
 }
 
